7ac0952711675e2f96ee9c19c6b264d94457ad5d
xgqfrms <xgqfrms@outlook.com>
2020-11-13 11:11:18 +0800

    Create console.js

---
+++ client/console.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-11-11
+ * @modified
+ *
+ * @description
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+const log = console.log;
+
+for(const key in console) {
+  log(`navigator.${key} =`, key, typeof key, console[key]);
+}
+
+
+// navigator.debug = debug string ƒ debug() { [native code] }
+// navigator.error = error string ƒ error() { [native code] }
+// navigator.info = info string ƒ info() { [native code] }
+// navigator.log = log string ƒ log() { [native code] }
+// navigator.warn = warn string ƒ warn() { [native code] }
+// navigator.dir = dir string ƒ dir() { [native code] }
+// navigator.dirxml = dirxml string ƒ dirxml() { [native code] }
+// navigator.table = table string ƒ table() { [native code] }
+// navigator.trace = trace string ƒ trace() { [native code] }
+// navigator.group = group string ƒ group() { [native code] }
+// navigator.groupCollapsed = groupCollapsed string ƒ groupCollapsed() { [native code] }
+// navigator.groupEnd = groupEnd string ƒ groupEnd() { [native code] }
+// navigator.clear = clear string ƒ clear() { [native code] }
+// navigator.count = count string ƒ count() { [native code] }
+// navigator.countReset = countReset string ƒ countReset() { [native code] }
+// navigator.assert = assert string ƒ assert() { [native code] }
+// navigator.profile = profile string ƒ profile() { [native code] }
+// navigator.profileEnd = profileEnd string ƒ profileEnd() { [native code] }
+// navigator.time = time string ƒ time() { [native code] }
+// navigator.timeLog = timeLog string ƒ timeLog() { [native code] }
+// navigator.timeEnd = timeEnd string ƒ timeEnd() { [native code] }
+// navigator.timeStamp = timeStamp string ƒ timeStamp() { [native code] }
+// navigator.context = context string ƒ context() { [native code] }
